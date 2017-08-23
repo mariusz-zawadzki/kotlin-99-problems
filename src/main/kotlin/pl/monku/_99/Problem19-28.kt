d8541f7a5ef23bf0cc987a80cdc799ecbc7abfe3
monku <mariusz.r.zawadzki@gmail.com>
2017-08-24 01:31:32 +0200

    add tests to solution 27

--- src/main/kotlin/pl/monku/_99/Problem19-28.kt
+++ src/main/kotlin/pl/monku/_99/Problem19-28.kt
@@ -5,34 +5,34 @@ import java.util.*
 
 
 //P19
-fun <T> rotate(n: Int, list: List<T>): List<T>{
+fun <T> rotate(n: Int, list: List<T>): List<T> {
     var rotate = n
-    if(rotate==0) {
+    if (rotate == 0) {
         return list
     }
 
     rotate %= list.size
-    if(rotate<0) {
+    if (rotate < 0) {
         rotate += list.size
     }
     val result = arrayListOf<T>()
-    result.addAll(list.subList(rotate,list.size))
-    result.addAll(list.subList(0,rotate))
+    result.addAll(list.subList(rotate, list.size))
+    result.addAll(list.subList(0, rotate))
     return result.toList()
 }
 
 //P20
-fun <T> removeAt(n: Int, list: List<T>): Pair<List<T>,T>{
+fun <T> removeAt(n: Int, list: List<T>): Pair<List<T>, T> {
     val toMutableList = list.toMutableList()
     val t = toMutableList.removeAt(n)
     return Pair(toMutableList, t)
 }
 
 //P21
-fun <T> insertAt(c: T, n: Int, list: List<T>): List<T>{
+fun <T> insertAt(c: T, n: Int, list: List<T>): List<T> {
     val toMutableList = list.toMutableList()
     toMutableList.removeAt(n)
-    toMutableList.add(n,c)
+    toMutableList.add(n, c)
     return toMutableList.toList()
 }
 
@@ -41,71 +41,66 @@ fun range(start: Int, end: Int): List<Int> = start.rangeTo(end).toList()
 
 val r = SecureRandom()
 //P23
-fun <E> randomSelect(n:Int, list: List<E>, random: Random = r): List<E>{
+fun <E> randomSelect(n: Int, list: List<E>, random: Random = r): List<E> {
     val mutList = list.toMutableList()
-    if(n>= list.size)
-    {
+    if (n >= list.size) {
         return list
     }
     val result = mutableListOf<E>()
-    while(result.size < n)
-    {
+    while (result.size < n) {
         result.add(mutList.removeAt(random.nextInt(mutList.size)))
     }
     return result.toList()
 }
 
 //P24
-fun randomSelect(n:Int, top: Int, random: Random = r): List<Int>{
+fun randomSelect(n: Int, top: Int, random: Random = r): List<Int> {
     val list = 1.rangeTo(top).toMutableList()
-    if(n>= list.size)
-    {
+    if (n >= list.size) {
         return list
     }
     val result = mutableListOf<Int>()
-    while(result.size < n)
-    {
+    while (result.size < n) {
         result.add(list.removeAt(random.nextInt(list.size)))
     }
     return result.toList()
 }
 
 //P25
-fun <E> randomPermute(list: List<E>, random: Random = r): List<E>
-{
+fun <E> randomPermute(list: List<E>, random: Random = r): List<E> {
     val result = list.toMutableList()
     Collections.shuffle(result, random)
     return result
 }
 
 //P26
-fun <E> combinations(n:Int,  list: List<E>): List<List<E>>
-{
-    return Combinations(n, list.size).allCombinations.map { combination->
+fun <E> combinations(n: Int, list: List<E>): List<List<E>> {
+    return Combinations(n, list.size).allCombinations.map { combination ->
         val result = mutableListOf<E>()
-        combination.forEach({el-> result.add(list[el])})
+        combination.forEach({ el -> result.add(list[el]) })
         result
     }
 }
+
 //P27
 fun <E> group(groups: List<Int>, list: List<E>): List<List<List<E>>> {
     val groupSize = groups.reduce({ a, b -> a + b })
-    check(list.size == groupSize, {"grouping size ($groupSize)should match list size (${list.size})"} )
+    check(list.size == groupSize, { "grouping size ($groupSize)should match list size (${list.size})" })
     val combinations = mutableListOf<List<List<Int>>>()
 
     var size = list.size
-    groups.forEach { e->
+    groups.forEach { e ->
         combinations.add(Combinations(e, size).allCombinations)
-        size -=e
+        size -= e
     }
     val groupedCombinations = groupCombinations(combinations)
-    return groupedCombinations.map { parentCombinations->
+    return groupedCombinations.map { parentCombinations ->
         val result = mutableListOf<List<E>>()
         val reminder = list.toMutableList()
-        parentCombinations.forEach { combination->
+        parentCombinations.forEach { combination ->
             val combinationResult = mutableListOf<E>()
             //sortDescending to avoid index out of bound
-            combination.sortedDescending().forEach({index->
+            combination.sortedDescending().forEach({ index ->
                 combinationResult.add(reminder.removeAt(index))
             })
             result.add(combinationResult)
@@ -114,20 +109,20 @@ fun <E> group(groups: List<Int>, list: List<E>): List<List<List<E>>> {
     }
 }
 
-fun groupCombinations(combination : List<List<List<Int>>>): List<List<List<Int>>>{
+fun groupCombinations(combination: List<List<List<Int>>>): List<List<List<Int>>> {
     var prefixes = listOf<List<List<Int>>>()
-    combination.forEach { combinationList->
+    combination.forEach { combinationList ->
         prefixes = groupCombinations1(prefixes, combinationList)
     }
     return prefixes
 }
 
 fun groupCombinations1(prefixes: List<List<List<Int>>>, combination: List<List<Int>>): List<List<List<Int>>> {
-    return if(prefixes.isEmpty()) {
-        combination.map { l-> listOf(l) }
+    return if (prefixes.isEmpty()) {
+        combination.map { l -> listOf(l) }
     } else {
-        prefixes.flatMap { prefix->
-            val map = combination.map {c ->
+        prefixes.flatMap { prefix ->
+            val map = combination.map { c ->
                 val toMutableList = prefix.toMutableList()
                 toMutableList.add(c)
                 toMutableList.toList()
@@ -139,26 +134,15 @@ fun groupCombinations1(prefixes: List<List<List<Int>>>, combination: List<List<I
 
 
 //P28
-fun <E> lengthSort(list: List<List<E>>): List<List<E>> = list.sortedBy { l->l.size }
+fun <E> lengthSort(list: List<List<E>>): List<List<E>> = list.sortedBy { l -> l.size }
 
-fun <E> lengthFreqSort(list: List<List<E>>): List<List<E>>
-{
+fun <E> lengthFreqSort(list: List<List<E>>): List<List<E>> {
     val frequencyMap = mutableMapOf<Int, Int>()
-    list.forEach({e->
+    list.forEach({ e ->
         val plus = frequencyMap[e.size]?.plus(1)
-        frequencyMap[e.size] = if(plus !=null) plus else 1
+        frequencyMap[e.size] = if (plus != null) plus else 1
     })
-    return list.sortedBy { l->frequencyMap[l.size] }
-
-}
-
-fun main(args: Array<String>) {
-//    println(lengthFreqSort(listOf("abc".toList(), "de".toList(), "fgh".toList(), "de".toList(), "ijkl".toList(), "mn".toList(), "o".toList())))
-
-    val message = group(listOf(2, 2, 5), listOf("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
-    message.forEach { m->
-        println(m)
-    }
+    return list.sortedBy { l -> frequencyMap[l.size] }
 
 }
 
@@ -170,11 +154,10 @@ class Combinations(val m: Int, val n: Int) {
         generate(0)
     }
 
-    private fun generate(k: Int){
+    private fun generate(k: Int) {
         if (k >= m) {
             allCombinations.add(combination.toList())
-        }
-        else {
+        } else {
             for (j in 0 until n)
                 if (k == 0 || j > combination[k - 1]) {
                     combination[k] = j
---
+++ src/test/kotlin/pl/monku/_99/Problem27Tests.kt
@@ -0,0 +1,69 @@
+package pl.monku._99
+
+import org.hamcrest.core.Is
+import org.junit.Assert
+import org.junit.Test
+
+
+class Problem27Tests{
+
+
+    @Test
+    fun `should group example value`() {
+        val list = listOf("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")
+        val groups = listOf(2, 2, 5)
+        val message = group(groups, list)
+
+        val size = calculateExpectedSize(groups, list)
+        Assert.assertTrue(message.size == size)
+    }
+
+    @Test
+    fun `should group small example`() {
+        val list = listOf("Aldo", "Beat", "Carla")
+        val groups = listOf(1,1,1)
+        val message = group(groups, list)
+
+        val size = calculateExpectedSize(groups, list)
+        Assert.assertTrue(message.size == size)
+    }
+
+    @Test
+    fun `should group small example with known result`() {
+        val A = "Aldo"
+        val B = "Beat"
+        val C = "Carla"
+        val list = listOf(A, B, C)
+        val groups = listOf(1,2)
+        val message = group(groups, list)
+
+        val size = calculateExpectedSize(groups, list)
+        Assert.assertTrue(message.size == size)
+
+        Assert.assertThat(message, Is.`is`(
+                listOf(
+                        listOf(listOf(A), listOf(C,B)),
+                        listOf(listOf(B), listOf(C,A)),
+                        listOf(listOf(C), listOf(B,A))
+        )
+        ))
+    }
+
+    @Test
+    fun `should group into one group`() {
+        val list = listOf("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida")
+        val groups = listOf(9)
+        val message = group(groups, list)
+
+        val size = calculateExpectedSize(groups, list)
+        Assert.assertTrue(message.size == size)
+    }
+
+    private fun calculateExpectedSize(groups: List<Int>, list: List<String>): Int {
+        var size = factorial(list.size)
+        groups.forEach { i -> size /= factorial(i) }
+        return size
+    }
+    private fun factorial(list: Int) = 1.rangeTo(list).reduce { a, b -> a*b }
+
+}
