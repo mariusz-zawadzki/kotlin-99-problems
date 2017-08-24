9f6f62843f609418f51d65b93e8838876a5139b9
monku <mariusz.r.zawadzki@gmail.com>
2017-08-24 19:52:21 +0200

    add some more solutions in a bad way (no tests)

---
+++ src/main/kotlin/pl/monku/_99/Problem31-39.kt
@@ -0,0 +1,125 @@
+package pl.monku._99
+
+//P31
+fun Int.isPrime(): Boolean{
+    if(this<= 1)
+        return false
+    else if(this <=3)
+        return true
+    else if(this % 2 == 0 || this % 3 == 0)
+        return false
+    var i = 5
+    while(i*i <= this )
+    {
+        if(this % i == 0 || this % (i+2) == 0)
+            return false
+        i +=6
+    }
+    return true
+}
+
+//P32
+fun gcd(i1: Int, i2: Int): Int
+{
+    var a = i1
+    var b = i2
+    while(b !=0){
+        val t = b
+        b = a % b
+        a = t
+    }
+
+    return a
+}
+
+
+//P33
+fun Int.isCoprime(n: Int): Boolean = 1 == gcd(this,n)
+
+//P34
+fun Int.totient(): Int = (1..this).count { this.isCoprime(it) }
+
+//P35
+fun Int.primeFactors(): List<Int>{
+    val result = mutableListOf<Int>()
+    var reminder = this
+    val primeCalculator = PrimesGenerator(2)
+    while(reminder!=1)
+    {
+        if(reminder% primeCalculator.currentPrime ==0)
+        {
+            reminder/=primeCalculator.currentPrime
+            result+=primeCalculator.currentPrime
+            primeCalculator.currentPrime
+        }
+        else
+        {
+            primeCalculator.nextPrime()
+        }
+        //is it an improvement? write tests!!
+        if(reminder.isPrime())
+        {
+            result.add(reminder)
+            reminder = 1
+        }
+
+    }
+    return result
+}
+
+
+//P36
+fun Int.primeFactorMultiplicity(): List<Pair<Int,Int>>{
+    val primeFactors = this.primeFactors()
+    val result = mutableMapOf<Int,Int>()
+    primeFactors.forEach {
+        val count = result.getOrDefault(it, 0)
+        result.put(it, count+1)
+    }
+    return result.map { Pair(it.key,it.value) }
+}
+
+//P37
+fun Int.phi(): Int
+{
+    val primeFactorMultiplicity = this.primeFactorMultiplicity()
+    return primeFactorMultiplicity.fold(1, {acc, p->
+        acc * (p.first-1)*p.first.pow(p.second-1)
+    })
+}
+
+//p38
+//ommited
+
+//P39
+fun listPrimesInRange(range: IntRange) = range.filter { it.isPrime() }
+
+fun Int.pow(n: Int): Int{
+    if(n == 0)
+    {
+        return 1
+    }
+    var result = 1
+    for(i in 1..n)
+    {
+        result*=this
+    }
+    return result
+}
+
+
+class PrimesGenerator(var currentPrime: Int)
+{
+    fun nextPrime(): Int
+    {
+        while(!(++currentPrime).isPrime()){}
+        return currentPrime
+    }
+}
+
+
+fun main(args: Array<String>) {
+    System.out.println(134.phi())
+    System.out.println(134.totient())
+    System.out.println(listPrimesInRange(7..31))
+}
---
+++ src/test/kotlin/pl/monku/_99/Problem31Tests.kt
@@ -0,0 +1,16 @@
+package pl.monku._99
+
+import org.junit.Assert
+import org.junit.Test
+
+class Problem31Tests {
+    @Test
+    fun `should find expected number of primes up to 1000`() {
+        var count = 0
+        for(i in 1..1000)
+        {
+            if(i.isPrime()) count++
+        }
+        Assert.assertEquals("Expected 168 primes in rante 1..1000", 168, count)
+    }
+}
