6b99b91dea0aae4a02993b5602d3253849baec8a
monku <mariusz.r.zawadzki@gmail.com>
2017-11-10 00:36:43 +0100

    add solution to problem 55 using cooroutines. (does not fix worst case, as it looks like a single threaded problem - for now)

--- src/main/kotlin/pl/monku/_99/Problem55.kt
+++ src/main/kotlin/pl/monku/_99/Problem55.kt
@@ -1,7 +1,24 @@
 package pl.monku._99
 
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
+import kotlin.coroutines.experimental.buildSequence
+import kotlin.system.measureTimeMillis
+
 fun main(args: Array<String>) {
-    balancedTrees(4, "X").toString().print()
+    val nodes = 127+129
+    val time = measureTimeMillis {
+        balancedTrees(nodes, "X").size.toString().print()
+    }
+    val timeCooroutines = measureTimeMillis {
+        runBlocking {
+            balancedTreesCooroutines(nodes, "X").size.toString().print()
+        }
+    }
+
+    println("normal      took $time ms")
+    println("cooroutines took $timeCooroutines ms")
 }
 
 //
@@ -14,13 +31,63 @@ fun <T> balancedTrees(nodes: Int, element: T): List<Tree<T>> {
         val n = (nodes - 1) / 2
         val balancedLeft = balancedTrees(n, element)
         val balancedRight = balancedTrees(nodes - 1 - n, element)
-        return balancedLeft.flatMap { left ->
+        val mapped = HashSet<Pair<Tree<T>,Tree<T>>>()
+        val flatMap = balancedLeft.flatMap { left ->
             balancedRight.flatMap { right ->
-                listOf(Node(element, left, right), Node(element, right, left))
+                flatMapTree(mapped, left, right, element)
             }
-        }.distinct()
-
-
+        }
+        return flatMap
     }
 }
 
+suspend fun <T> balancedTreesCooroutines(nodes: Int, element: T): List<Tree<T>> {
+    if (nodes == 0) {
+        return listOf(End)
+    } else if (nodes == 1) {
+        return listOf(Node(element))
+    } else {
+        val n = (nodes - 1) / 2
+
+        val asyncBalancedLeft = async(CommonPool) {
+            balancedTreesCooroutines(n, element)
+        }
+        val asyncBalancedRight = async(CommonPool) {
+            balancedTreesCooroutines(nodes - 1 - n, element)
+        }
+        val balancedLeft = asyncBalancedLeft.await()
+        val balancedRight = asyncBalancedRight.await()
+
+        val mapped = HashSet<Pair<Tree<T>,Tree<T>>>()
+        val mappedSequence = buildSequence {
+            balancedLeft.forEach { left ->
+                balancedRight.forEach { right ->
+                    yieldAll(flatMapTree(mapped, left, right, element))
+                }
+            }
+        }
+        var toList = listOf<Tree<T>>()
+        val time = measureTimeMillis {
+            toList = mappedSequence.toList()
+        }
+        "balancedRight size: ${balancedRight.size}".print()
+        "balancedLeft  size: ${balancedLeft.size}".print()
+        "to list took $time".print()
+        return toList
+    }
+}
+
+private fun <T> flatMapTree(mapped: HashSet<Pair<Tree<T>, Tree<T>>>, left: Tree<T>, right: Tree<T>, element: T): List<Node<T>> {
+    val didHaveLeftRight = mapped.add(Pair(left, right))
+    val didHaveRightLeft=  mapped.add(Pair(right, left))
+    val didNotHaveThisPair = didHaveLeftRight || didHaveRightLeft
+    return if (didNotHaveThisPair) {
+        if (left == right) {
+            listOf(Node(element, left, right))
+        } else {
+            listOf(Node(element, left, right), Node(element, right, left))
+        }
+    } else {
+        listOf()
+    }
+}
--- src/test/kotlin/pl/monku/_99/Problem55KtTest.kt
+++ src/test/kotlin/pl/monku/_99/Problem55KtTest.kt
@@ -1,5 +1,6 @@
 package pl.monku._99
 
+import kotlinx.coroutines.experimental.runBlocking
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.hasItems
 import org.junit.Assert.*
@@ -10,10 +11,11 @@ fun x(l : Tree<String> =  End, r : Tree<String> = End): Tree<String> = Node("x",
 class P55Test {
 
 
+    @Suppress("UNCHECKED_CAST")
     @Test
     fun `construct all balanced trees`() {
         assertThat(balancedTrees(0, "x").size, equalTo(1))
-        assertThat(balancedTrees(0, "x").get(0) as Tree<Nothing>, equalTo(End))
+        assertThat(balancedTrees(0, "x")[0] as Tree<Nothing>, equalTo(End))
         assertThat(balancedTrees(1, "x"), hasItems(x()))
         val listOf = listOf(
                 x(l= End, r = x()),
@@ -33,6 +35,35 @@ class P55Test {
                 x(x(l = x(), r = End),x(l = End, r = x())),
                 x(x(l = x(), r = End),x(l = x(), r = End))
         )))
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    @Test
+    fun `construct all balanced trees with cooroutine implementation`() {
+        runBlocking {
+
+            assertThat(balancedTreesCooroutines(0, "x").size, equalTo(1))
+            assertThat(balancedTreesCooroutines(0, "x")[0] as Tree<Nothing>, equalTo(End))
+            assertThat(balancedTreesCooroutines(1, "x"), hasItems(x()))
+            val listOf = listOf(
+                    x(l = End, r = x()),
+                    x(l = x(), r = End)
+            )
+            assertThat(balancedTreesCooroutines(2, "x"), equalTo(listOf))
+            assertThat(balancedTreesCooroutines(3, "x"), equalTo(listOf(x(x(), x()))))
+            assertThat(balancedTreesCooroutines(4, "x"), equalTo(listOf(
+                    x(x(l = End, r = End), x(l = End, r = x())),
+                    x(x(l = End, r = x()), x(l = End, r = End)),
+                    x(x(l = End, r = End), x(l = x(), r = End)),
+                    x(x(l = x(), r = End), x(l = End, r = End))
+            )))
+            assertThat(balancedTreesCooroutines(5, "x"), equalTo(listOf(
+                    x(x(l = End, r = x()), x(l = End, r = x())),
+                    x(x(l = End, r = x()), x(l = x(), r = End)),
+                    x(x(l = x(), r = End), x(l = End, r = x())),
+                    x(x(l = x(), r = End), x(l = x(), r = End))
+            )))
+        }
     }
 
     companion object {
